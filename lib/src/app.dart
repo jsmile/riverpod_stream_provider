c448c07b5079f76c2b184fd9f8e997469b3787b9
jsmile <jsmile@naver.com>
2023-12-14 17:07:29 +0900

    riverpod Stream Proivder starter commit

---
+++ lib/src/app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'pages/ticker/ticker_page.dart';
+import 'widgets/cutom_button.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stream Provider',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        shrinkWrap: true,
+        children: const [
+          CustomButton(
+            title: 'Ticker',
+            child: TickerPage(),
+          ),
+        ],
+      ),
+    );
+  }
+}
